0e0e4abd3704eaffc69b4e939033a401d16cbb2b
amd1250x <aaron.m.dsa@gmail.com>
2016-05-18 16:48:36 -0500

    Adjusted code for custom forms

--- MyFitness/forms.py
+++ MyFitness/forms.py
@@ -4,6 +4,10 @@ from django import forms
 
 
 class UserForm(ModelForm):
+    username = forms.CharField(label='Username', max_length=100)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput())
+    email = forms.EmailField(label='Email', max_length=254)
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
