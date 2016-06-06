a1492b9ff86e454e379190f7ec39a26683a310a2
amd1250x <aaron.m.dsa@gmail.com>
2016-06-06 12:26:43 -0500

    Added CSS file, fixed formatting on home log page.

--- MyFitness/views.py
+++ MyFitness/views.py
@@ -20,6 +20,7 @@ def index(request):
     class Day:
         def __init__(self, date):
             self.date = date
+            self.display = self.date.strftime("%A - %B %d, %Y")
             self.has_items = False
 
     days_pm2 = [Day(today - datetime.timedelta(2)),
