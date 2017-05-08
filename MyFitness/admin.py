a1eebba2bea10d7565855eb21828bf205de0f23e
amd1250x <aaron.m.dsa@gmail.com>
2017-05-07 23:18:40 -0500

    Updated admin.py

--- MyFitness/admin.py
+++ MyFitness/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from models import FitnessLog, BodyWeightLog
+from models import FitnessLog, BodyWeightLog, Workout, WorkoutExercise, WorkoutLog
 # Register your models here.
 
 
 admin.site.register(FitnessLog)
 admin.site.register(BodyWeightLog)
+admin.site.register(Workout)
+admin.site.register(WorkoutExercise)
+admin.site.register(WorkoutLog)
